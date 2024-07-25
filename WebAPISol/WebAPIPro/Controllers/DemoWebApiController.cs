077ee73a309bf836b064569a32c4cf6262c28bc4
Arun Kumar Patnana <arunkumar.vtalent@gmail.com>
2024-07-25 09:41:06 +0530

    A new controller created for Airthmatic Operations

---
+++ WebAPISol/WebAPIPro/Controllers/DemoWebApiController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace WebAPIPro.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DemoWebApiController : ControllerBase
+    {
+        [HttpGet]
+        [Route("Addition")]
+        public IActionResult Addition(int x, int y)
+        {
+            try
+            {
+                return Ok(x + y);
+            }
+            catch (Exception Ex)
+            {
+                return BadRequest(Ex.Message);
+            }
+        }
+    }
+}
